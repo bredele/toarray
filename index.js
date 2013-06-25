0799540c0c191003880b692876bed93b93db2f54
bredele <olivier@petrofeed.com>
2013-06-24 21:29:45 -0600

    add slice parameter

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ module.exports = toArray;
 
 /**
  * toArray
+ *
+ * @param {Object}  obj Array-like or string
+ * @param {Number}  index slice index
  * @return {Array} Empty Array if argument other than string or Object
  * @api public
  */
 
-function toArray(like) {
-  return [].slice.call(like);
+function toArray(obj, index) {
+  return [].slice.call(obj).slice(index);
 }
